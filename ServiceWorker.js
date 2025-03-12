e12a77ab91e6b5cf72973903e2479f16c5b8625b
JeneHattingh <165263243+JeneHattingh@users.noreply.github.com>
2025-03-12 11:38:44 +0200

    WebGL App

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "JNZ_Rkk-RRK Configurator-0.0.1";
+const contentToCache = [
+    "Build/rkk_compressed.loader.js",
+    "Build/rkk_compressed.framework.js.gz",
+    "Build/rkk_compressed.data.gz",
+    "Build/rkk_compressed.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
